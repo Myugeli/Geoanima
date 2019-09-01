8d0f5ac6115631422309a3ee205b5bfd1e3fed1e
myuey <myuey@MSI.attlocal.net>
2019-09-01 16:37:29 -0400

    Testing

---
+++ GeoAnima/src/entities/Point.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class Point {
+	private int xPos;
+	private int yPos;
+	private boolean isPen;
+	private boolean isFixed;
+	public Point(int xPos, int yPos, boolean isPen, boolean isFixed) {
+		
+	}
+}
